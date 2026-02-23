4441681bb5b479c4721b2ef3cc5ad053c5f4b570
Behradjahangiri <behrad.jager@gmail.com>
2026-02-23 16:04:38 +0330

    class Transaction

--- src/main/java/model/entity/Transaction.java
+++ src/main/java/model/entity/Transaction.java
@@ -1,4 +1,24 @@
 package model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+
+
 public class Transaction {
+    private long transactionid;
+    private TransactionType transactionType;
+    private double amount;
+    private LocalDate date;
+    private Account sourceAccount;
+    private Account targetAccount;
+
 }
