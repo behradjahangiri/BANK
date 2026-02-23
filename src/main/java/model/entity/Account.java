6c459c75e4930000e99827ae9f74be85c320d5bf
Behradjahangiri <behrad.jager@gmail.com>
2026-02-23 16:41:08 +0330

    class Account customer transaction entity created no method

--- src/main/java/model/entity/Account.java
+++ src/main/java/model/entity/Account.java
@@ -1,4 +1,44 @@
 package model.entity;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+
+
+//کلاس میتونه ابسترک بشه
+
 public class Account {
+    private String accountName;
+    private double balance;
+    private LocalDate openDate;
+    private String status;
+    private Customer customer;
+
+//    class list check bashe
+
+    private List<Transaction> transactions;
+
+
+    private Account deposit(){
+        return null;
+    }
+
+    private Account withdraw(){
+        return null;
+    }
+
+    private void addTransaction(){
+    }
+
+    private double chech(){
+        return this.balance;
+    }
+
 }
--- src/main/java/model/entity/Customer.java
+++ src/main/java/model/entity/Customer.java
@@ -1,4 +1,40 @@
 package model.entity;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+
 public class Customer {
+    private long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String nationllid;
+    private String phone;
+    private String email;
+    private String address;
+    private LocalDate dateOfBirth;
+    private LocalDate registrationDate;
+    private List<Account> accounts;
+    private List<Loan> loans;
+
+    private void createAccount() {
+    }
+    private void updateAccount() {
+    }
+    private void deleteAccount() {
+    }
+    private void getAccount() {
+    }
+    private void requestLoan(){
+    }
+
 }
