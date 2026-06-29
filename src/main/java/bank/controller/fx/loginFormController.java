b0d24f7ce7fb7d55920a65f12ee65805fdd4f4d9
Behradjahangiri <behrad.jager@gmail.com>
2026-06-29 23:29:32 +0330

    connected to view

--- src/main/java/bank/controller/fx/loginFormController.java
+++ src/main/java/bank/controller/fx/loginFormController.java
@@ -1,17 +1,19 @@
-package controller.fx;
+package bank.controller.fx;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import lombok.extern.slf4j.Slf4j;
-
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 @Slf4j
 public class loginFormController implements Initializable {
     @FXML
-    private TextField usernameTextField, passwordTextField;
+    private TextField userNameTextField;
+    @FXML
+    private TextField passwordTextField;
     @FXML
     private Button loginButton;
 
