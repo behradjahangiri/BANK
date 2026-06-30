cc28242280d1627af7ec06128876290d86c8ecb6
Behradjahangiri <behrad.jager@gmail.com>
2026-07-01 01:59:32 +0330

    AccountSelectionFormController Table Data coded

--- src/main/java/bank/controller/fx/AccountSelectionFormController.java
+++ src/main/java/bank/controller/fx/AccountSelectionFormController.java
@@ -1,15 +1,20 @@
 package bank.controller.fx;
 
+import bank.controller.AccountController;
 import bank.model.entity.Account;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.URL;
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.ResourceBundle;
 
 @Slf4j
@@ -28,7 +33,7 @@ public class AccountSelectionFormController implements Initializable {
     private TableColumn<Account, Double> balanceColumn;
 
     @FXML
-    private TableColumn<Account, Date> openDateColumn;
+    private TableColumn<Account, LocalDate> openDateColumn;
 
     @FXML
     private TableColumn<Account, String> statusColumn;
@@ -36,5 +41,20 @@ public class AccountSelectionFormController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         log.info("AccountSelectionFormController loaded");
+//        showDataOnTableView(AccountController.getInstance().findAll());
     }
+
+
+    private void showDataOnTableView(List<Account> accountList){
+
+        ObservableList<Account> accountObservableList = FXCollections.observableList(accountList);
+        idColumn.setCellValueFactory(new PropertyValueFactory<Account,Integer>("accountId"));
+        balanceColumn.setCellValueFactory(new PropertyValueFactory<Account,Double>("balance"));
+        openDateColumn.setCellValueFactory(new PropertyValueFactory<Account, LocalDate>("openDate"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<Account, String>("status"));
+        accountTable.setItems(accountObservableList);
+        accountTable.getItems().clear();
+    }
+
+
 }
