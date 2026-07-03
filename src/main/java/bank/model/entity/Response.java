a53d9dc062c6a63a57c7a8202f75d2d1c4704037
Behradjahangiri <behrad.jager@gmail.com>
2026-07-03 14:35:41 +0330

    comment

--- src/main/java/bank/model/entity/Response.java
+++ src/main/java/bank/model/entity/Response.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class Response {
     private ResponseStatus responseStatus;
     private String message;
-    private Object data;
+    private Object data; // برای خود ابجکی که کار روز انحام شده استفاده میشه
 
     public Response(ResponseStatus responseStatus, String message) {
         this.responseStatus = responseStatus;
