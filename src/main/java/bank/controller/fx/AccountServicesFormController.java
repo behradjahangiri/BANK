6ed379bab63adee851033ae2631638b27626f73b
Behradjahangiri <behrad.jager@gmail.com>
2026-07-05 23:50:10 +0330

    AccountServicesFormController transactionButton

--- src/main/java/bank/controller/fx/AccountServicesFormController.java
+++ src/main/java/bank/controller/fx/AccountServicesFormController.java
@@ -1,10 +1,14 @@
 package bank.controller.fx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,7 +26,17 @@ public class AccountServicesFormController implements Initializable {
         log.info("AccountServicesFormController loaded");
         withdrawButton.setOnAction(event -> {});
         transferButton.setOnAction(event -> {});
-        transactionButton.setOnAction(event -> {});
+        transactionButton.setOnAction(event -> {
+            try {
+                Scene scene = new Scene(
+                        FXMLLoader.load(getClass().getResource("/view/Transaction.fxml"))
+                );
+            } catch (IOException e) {
+//                    Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid username or password", ButtonType.OK);
+//                    alert.show();
+                throw new RuntimeException(e);
+            }
+        });
         backButton.setOnAction(event -> {});
     }
 }
