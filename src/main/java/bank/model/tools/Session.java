f8a3021f89ce7b63cd26ff1391dbf6083bbeb8d1
Behradjahangiri <behrad.jager@gmail.com>
2026-07-08 20:46:09 +0330

    Session creation

---
+++ src/main/java/bank/model/tools/Session.java
@@ -0,0 +1,9 @@
+package bank.model.tools;
+
+import bank.model.entity.Account;
+
+public class Session {
+    public static Integer accountId;
+    public static Double balance;
+    public static Account account;
+}
