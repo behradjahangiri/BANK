83cb1783a53f7e44a09008d13989699c71a926aa
Behradjahangiri <behrad.jager@gmail.com>
2026-07-10 23:46:59 +0330

    Seeion accountId = > customerId lombok @

--- src/main/java/bank/model/tools/Session.java
+++ src/main/java/bank/model/tools/Session.java
@@ -1,9 +1,17 @@
 package bank.model.tools;
 
 import bank.model.entity.Account;
+import lombok.*;
+
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
 
 public class Session {
-    public static Integer accountId;
+    public static Integer customerId;
     public static Double balance;
     public static Account account;
 }
