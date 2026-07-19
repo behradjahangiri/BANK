85ae1fd9c18ce848670194150feb27eab76df969
Behradjahangiri <behrad.jager@gmail.com>
2026-07-19 13:33:07 +0330

    backButton

--- src/main/java/bank/controller/fx/TransactionsFormController.java
+++ src/main/java/bank/controller/fx/TransactionsFormController.java
@@ -2,13 +2,14 @@ package bank.controller.fx;
 
 import bank.model.entity.Account;
 import bank.model.entity.Transaction;
+import bank.model.tools.FormLoader;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
-import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
@@ -16,6 +17,7 @@ import java.util.ResourceBundle;
 @Slf4j
 
 public class TransactionsFormController implements Initializable {
+
 
     @FXML
     private TableView<Transaction> transactionsTable;
@@ -38,8 +40,22 @@ public class TransactionsFormController implements Initializable {
     @FXML
     private Button backButton;
 
+    private final FormLoader formLoader = new FormLoader();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        log.info("transactionsFormController loaded");
+        backButton.setOnAction(actionEvent -> {
+            try {
+                formLoader.showFormAccountServices();
+                Stage stage = (Stage) backButton.getScene().getWindow();
+                stage.close();
+            } catch (IOException e) {
+                log.error("Error loading FormAccountSelection", e);
+                Alert alert = new Alert(Alert.AlertType.ERROR,"connot open FormAccountSelection", ButtonType.OK);
+                alert.showAndWait();
+            }
+        });
 
     }
 }
